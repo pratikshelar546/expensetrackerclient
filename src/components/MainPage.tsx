d92cd91d416b6d872412996376ee211e92c37acf
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2024-11-06 00:39:51 +0530

    lading page updating

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -6,16 +6,17 @@ import { BackgroundBeams } from "../CommonComponent/UI/background-beans";
 import * as motion from "framer-motion/client";
 import Image from "next/image";
 import sideImage from "../assets/icons/image.png";
+import { Spotlight } from "@/CommonComponent/UI/Spotlight";
 const MainPage = () => {
   return (
     <main className="w-full min-h-screen flex flex-col items-center overflow-x-hidden overflow-y-hidden">
-      <section className="relative w-full h-full min-h-[calc(100vh-3.6rem)] flex flex-col items-center justify-start border-b border-blue-500/50 mt-14">
         {/* <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="#ffe4e4"
+        className="top-40 left-0 md:left-60 md:-top-20"
+        fill="wheat"
         /> */}
+      <section className="relative w-full h-full min-h-[calc(100vh-3.6rem)] flex flex-col items-center justify-start border-b border-blue-500/50 mt-14 max-w-6xl">
         {/* -z-10 absolute -top-10 left-[calc(40%)] w-[100%] h-[calc(100vh-12rem)] */}
-        <div className="-z-10 absolute left-[calc(28%)] w-[100%] h-[calc(100vh-12rem)]">
+        <div className="-z-10 absolute left-[calc(20%)] w-[100%] h-[calc(100vh-12rem)]">
           <div className="absolute inset-0 flex justify-center items-center">
             <Image
               src={sideImage}
@@ -25,24 +26,26 @@ const MainPage = () => {
           </div>
           <div className="absolute inset-0 imggradiant"></div>
         </div>
-        <div className="text-white w-2xl max-h-[calc(100vh-16rem)] grow max-w-6xl px-4 mx-4 flex flex-col justify-center gap-6">
-          <motion.h1
-            initial={{ y: -50, x: -50, opacity: 0 }}
-            animate={{ y: 0, x: 0, opacity: 1 }}
-            transition={{ ease: "easeInOut", duration: 0.75 }}
-            className="text-5xl font-extrabold  px-4 text-left"
-          >
-            Manage your expenses
-          </motion.h1>
-          <motion.p
-            initial={{ y: -50, x: -50, opacity: 0 }}
-            animate={{ y: 0, x: 0, opacity: 1 }}
-            transition={{ ease: "easeInOut", duration: 0.75 }}
-            className="text-xl font-extrabold text-left px-4"
-          >
-            Streamline finances with ExpenseWise Track,categorize,and manage
-            expenses easily
-          </motion.p>
+        <div className="text-white max-h-[calc(100vh-16rem)] w-full grow px-4 mx-4 flex flex-col justify-center gap-6">
+          <div className="w-full max-w-xl">
+            <motion.h1
+              initial={{ y: -50, x: -50, opacity: 0 }}
+              animate={{ y: 0, x: 0, opacity: 1 }}
+              transition={{ ease: "easeInOut", duration: 0.75 }}
+              className="text-5xl font-extrabold  px-4 text-left"
+            >
+              Manage your expenses
+            </motion.h1>
+            <motion.p
+              initial={{ y: -50, x: -50, opacity: 0 }}
+              animate={{ y: 0, x: 0, opacity: 1 }}
+              transition={{ ease: "easeInOut", duration: 0.75 }}
+              className="text-xl font-extrabold text-left px-4"
+            >
+              Streamline finances with ExpenseWise Track,categorize,and manage
+              expenses easily
+            </motion.p>
+          </div>
         </div>
         {/* <div className="flex max-w-6xl items-center justify-between h-[80vh] z-[1] w-full">
         <div className="relative w-full h-full flex items-center">
